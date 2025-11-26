e472250a9f9979721429c012c9ad90a3e88cf200
Jyot Guransh <jyot.gurnansh.dua@oracle.com>
2025-11-26 23:24:43 +0530

    new

---
+++ MediTrackManager/src/main/java/org/meditrack/app/entity/Person.java
@@ -0,0 +1,66 @@
+package org.meditrack.app.entity;
+
+import java.util.UUID;
+
+public abstract class Person {
+    String name;
+    String age;
+    String address;
+    String ContactNo;
+    String email;
+    String id;
+
+    public Person(String name, String age, String address, String ContactNo, String email, String id) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.ContactNo = ContactNo;
+        this.email = email;
+        this.id = getUniqueID();
+    }
+
+    public String getUniqueID()
+    {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getContactNo() {
+        return ContactNo;
+    }
+    public void setContactNo(String contactNo) {
+        ContactNo = contactNo;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
+
